29306443c3895ba54888e605d0ec6cb0e2d46a57
lhjwork <jin.come.up.business@gmail.com>
2023-11-18 21:21:25 +0900

    yarn add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello CherryBlam</h1>;
+  return <div className="container">Hello CherryBlam</div>;
 };
 
 export default App;
