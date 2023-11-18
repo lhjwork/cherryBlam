9b4d4fb8dfbe3e59ddb1fd45a37920903b235cfa
lhjwork <jin.come.up.business@gmail.com>
2023-11-18 22:20:02 +0900

    30:01 routing 적용

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,20 @@
+import { Outlet, Navigate } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+  return (
+    <div>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default AuthLayout;
